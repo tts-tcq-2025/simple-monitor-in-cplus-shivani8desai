14ca2ca7be699ce8ee0797a39a491875d0a4bcd6
shivani8desai <41742187+shivani8desai@users.noreply.github.com>
2025-09-30 14:36:10 +0530

    Update Battery.cpp

--- Battery.cpp
+++ Battery.cpp
@@ -1,16 +1,36 @@
 #include<iostream>
 #include "Battery.hpp"
 
-bool batteryIsOk(float temperature, float soc, float chargeRate) {
-  if(temperature < 0 || temperature > 45) {
+bool BatteryTemp(float temperature ) 
+{
+  if(temperature < 0 || temperature > 45) 
+  {
     cout << "Temperature out of range!\n";
     return false;
-  } else if(soc < 20 || soc > 80) {
-    cout << "State of Charge out of range!\n";
-    return false;
-  } else if(chargeRate > 0.8) {
-    cout << "Charge Rate out of range!\n";
-    return false;
   }
-  return true;
 }
+  
+  bool BatteryCharge (float soc)
+  {
+    if(soc < 20 || soc > 80) 
+    {
+      cout << "State of Charge out of range!\n";
+      return false;
+    }
+  }
+
+  bool BatteryChargeRate(float chargeRate)
+  {
+    if(chargeRate > 0.8) 
+    {
+      cout << "Charge Rate out of range!\n";
+      return false;
+    }
+  }
+
+bool BatteryIsOK(float temperature, float soc,float chargeRate )
+{
+  return( BatteryTemp(temperature) && BatteryCharge (soc) && BatteryChargeRate(chargeRate) );
+}
+
+
