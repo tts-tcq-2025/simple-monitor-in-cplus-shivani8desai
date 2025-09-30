061cced02035dab4d6c7df4d15d08a036b0b600d
shivani8desai <41742187+shivani8desai@users.noreply.github.com>
2025-09-30 14:37:26 +0530

    Update Battery.hpp

--- Battery.hpp
+++ Battery.hpp
@@ -4,5 +4,9 @@
 using namespace std;
 
 bool batteryIsOk(float temperature, float soc, float chargeRate);
+bool BatteryTemp(float temperature );
+bool BatteryCharge (float soc); 
+bool BatteryChargeRate(float chargeRate);
+
 
 #endif
